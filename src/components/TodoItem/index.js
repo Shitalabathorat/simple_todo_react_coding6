e71a46b6ffc9c710d44e51f14325db42c6676df1
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-08 21:17:40 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,24 +1,4 @@
 
-import '.index.css'
-
-//import todoList from './components/SimpleTodos'
-
-const TodoItem = props => {
-  const {todoDetails, deleteUser} = props
-  const {title, id} = todoDetails
-
-  const onDelete () => {
-     deleteUser(id)
-  }
-
-  return (
-    <li className="user-card-container">
-      <h2>{title}</h2>
-      <button type="button" className="btn" onClick={onDelete}>Delete</button>
-    </li>
-  )
-}
-export default todoItem
 
 import './index.css'
 
