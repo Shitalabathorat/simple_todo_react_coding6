be3a04e9ba22dc5241f29d275009f6759e8ceaae
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-08 21:11:54 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -19,3 +19,24 @@ const TodoItem = props => {
   )
 }
 export default DestinationItem
+
+import './index.css'
+
+const todoItem = props => {
+  const {userDetails, deleteUser} = props
+  const {title, id} = userDetails
+
+  const onDelete = () => {
+    deleteUser(id)
+  }
+
+  return (
+    <li className="list">
+      <p>{title}</p>
+      <button className="btn" onClick={onDelete} type="button">
+        Delete
+      </button>
+    </li>
+  )
+}
+export default todoItem
