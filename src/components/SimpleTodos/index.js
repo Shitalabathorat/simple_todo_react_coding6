1beb60e6a3cca7d859146fa7609e4bdc8f8f4733
Shitalabathorat <126850348+Shitalabathorat@users.noreply.github.com>
2023-04-08 21:14:27 +0530

    Update index.js

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -47,7 +47,7 @@ class SimpleTodos extends Component {
         const  {todoList}=this.state
         const filteredUserData=todoList.filter(eachItem=>eachItem.id!==id)
         this.setState({
-            todoList=filteredUserData
+            todoList:filteredUserData
         })
     }
 
@@ -59,6 +59,82 @@ class SimpleTodos extends Component {
           <ul className="todo-list">
             {todoList.map(eachItem => (
               <TodoItem todoDetails={eachItem} deleteUser={this.deleteUser} key={eachItem.id} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default SimpleTodos
+
+import {Component} from 'react'
+import todoItem from '../TodoItem'
+import './index.css'
+
+const initialTodoList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+// Write your code here
+class SimpleTodos extends Component {
+  state = {todoList: initialTodoList}
+
+  deleteUser = id => {
+    const {todoList} = this.state
+    const filterUserDetailsList = todoList.filter(
+      eachUser => eachUser.id !== id,
+    )
+    this.setState({
+      todoList: filterUserDetailsList,
+    })
+  }
+
+  render() {
+    const {todoList} = this.state
+
+    return (
+      <div className="container">
+        <div className="cart">
+          <h1 className="head">Simple Todos</h1>
+          <ul className="list-container">
+            {todoList.map(eachUser => (
+              <todoItem
+                userDetails={eachUser}
+                deleteUser={this.DeleteUser}
+                key={eachUser.id}
+              />
             ))}
           </ul>
         </div>
